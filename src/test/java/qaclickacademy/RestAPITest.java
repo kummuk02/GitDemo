9ef32fbe0ca2de98ee7ba6c8a674581543921b01
mukesh <mantu0231@gmail.com>
2022-02-26 09:07:05 -0600

    this is commit from Asian guy

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -10,6 +10,13 @@ public class RestAPITest {
 		System.out.println("PostJira");
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
+		//fgrf
+		//kiyr
+		//otes
+		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
 	}
 	
 	@Test
